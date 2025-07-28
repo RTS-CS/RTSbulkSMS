b0a970b6fb4cd888baf33b442408d23503826a6d
RTS-CS <sweigardka@cityofgainesville.org>
2025-07-28 14:00:36 -0400

    Create sendsms.py

---
+++ sendsms.py
@@ -0,0 +1,28 @@
+import os
+from twilio.rest import Client
+
+# Load credentials from Render environment variables
+TWILIO_SID = os.getenv("TWILIO_SID")
+TWILIO_AUTH = os.getenv("TWILIO_AUTH")
+TWILIO_PHONE = os.getenv("TWILIO_PHONE")
+
+client = Client(TWILIO_SID, TWILIO_AUTH)
+
+# List of recipient phone numbers
+recipients = [
+    "+1xxxxxxxxxx",
+    "+1xxxxxxxxxx",
+    "+1xxxxxxxxxx"
+]
+
+# Message to send
+message_body = "Hello! This is a test message from your Render-deployed Python app 🎉"
+
+# Send the SMS
+for recipient in recipients:
+    message = client.messages.create(
+        to=recipient,
+        from_=TWILIO_PHONE,
+        body=message_body
+    )
+    print(f"Sent to {recipient}: {message.sid}")
